ac045281ecba37bf33b6ad28f4ed19e522d8bee1
Noah Friedman <noahfriedman2@gmail.com>
2021-08-01 11:56:49 -0400

    Remove handlers variable, CreateRouter will manually route [skip ci]

--- main.go
+++ main.go
@@ -2,20 +2,12 @@ package main
 
 import (
 	"github.com/gorilla/mux"
-	"net/http"
-
 	unit "unit.nginx.org/go"
 )
-
-var handlers = make(map[string]http.Handler)
 
 // CreateRouter routes declared handlers to their respective paths.
 func CreateRouter() *mux.Router {
 	r := mux.NewRouter()
-
-	for p, h := range handlers {
-		r.PathPrefix(p).Handler(h)
-	}
 
 	return r
 }
